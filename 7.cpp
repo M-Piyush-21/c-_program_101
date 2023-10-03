ad2775c50ef71d55a64db9b88e54256de0fb639a
Mahajan Piyush <142775278+M-Piyush-21@users.noreply.github.com>
2023-10-04 02:10:57 +0530

    7.cpp

--- 7.cpp
+++ 7.cpp
@@ -11,6 +11,9 @@ int main ()
         if(n % i == 0)
             sum = sum + i;
     }
+// The smaller numbers that can divide evenly into 6 (except for 6 itself) are 1, 2, and 3.
+// When you add up 1 + 2 + 3, you get 6, which is the same as the original number.
+// So, 6 is a perfect number! 
     
     if(sum == n)
         cout << n << " is a perfect number";
