cb4055674baadcd067d797515efb6a2bc87c38a1
Mahajan Piyush <142775278+M-Piyush-21@users.noreply.github.com>
2023-10-04 16:00:51 +0530

    67_two_matrix.cpp

--- 67_two_matrix.cpp
+++ 67_two_matrix.cpp
@@ -1,49 +1,57 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int r, c, a[100][100], b[100][100], sum[100][100], i, j;
+int main() {
+    int rows1, cols1, rows2, cols2;
 
-    cout << "Enter number of rows (between 1 and 100): ";
-    cin >> r;
+    // Input dimensions of the first matrix
+    cout << "Enter the number of rows and columns for the first matrix: ";
+    cin >> rows1 >> cols1;
 
-    cout << "Enter number of columns (between 1 and 100): ";
-    cin >> c;
+    // Input dimensions of the second matrix
+    cout << "Enter the number of rows and columns for the second matrix: ";
+    cin >> rows2 >> cols2;
 
-    cout << endl << "Enter elements of 1st matrix: " << endl;
+    // Check if the dimensions are compatible for addition
+    if (rows1 != rows2 || cols1 != cols2) {
+        cout << "Matrices cannot be added. Dimensions are not compatible." << endl;
+        return 1;
+    }
 
-    // Storing elements of first matrix entered by user.
-    for(i = 0; i < r; ++i)
-       for(j = 0; j < c; ++j)
-       {
-           cout << "Enter element a" << i + 1 << j + 1 << " : ";
-           cin >> a[i][j];
-       }
+    // Create matrices and initialize them
+    int mat1[rows1][cols1], mat2[rows2][cols2];
 
-    // Storing elements of second matrix entered by user.
-    cout << endl << "Enter elements of 2nd matrix: " << endl;
-    for(i = 0; i < r; ++i)
-       for(j = 0; j < c; ++j)
-       {
-           cout << "Enter element b" << i + 1 << j + 1 << " : ";
-              cin >> b[i][j];
-       }
-
-    // Adding Two matrices
-    for(i = 0; i < r; ++i)
-        for(j = 0; j < c; ++j)
-            sum[i][j] = a[i][j] + b[i][j];
-
-    // Displaying the resultant sum matrix.
-    cout << endl << "Sum of two matrix is: " << endl;
-    for(i = 0; i < r; ++i)
-        for(j = 0; j < c; ++j)
-        {
-            cout << sum[i][j] << "  ";
-            if(j == c - 1)
-                cout << endl;
+    cout << "Enter elements of the first matrix:" << endl;
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols1; j++) {
+            cin >> mat1[i][j];
         }
+    }
+
+    cout << "Enter elements of the second matrix:" << endl;
+    for (int i = 0; i < rows2; i++) {
+        for (int j = 0; j < cols2; j++) {
+            cin >> mat2[i][j];
+        }
+    }
+
+    // Add the matrices
+    int sum[rows1][cols1];
+
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols1; j++) {
+            sum[i][j] = mat1[i][j] + mat2[i][j];
+        }
+    }
+
+    // Print the result
+    cout << "The result of addition is:" << endl;
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols1; j++) {
+            cout << sum[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
